99acd4e67d14cee3b935d8c0e3a3314ab6a6beee
galaxysailing <galaxysailing@163.com>
2022-06-25 23:01:05 +0800

    add a-trous wavelet

--- homework5/src/denoiser.cpp
+++ homework5/src/denoiser.cpp
@@ -88,18 +88,78 @@ void Denoiser::TemporalAccumulation(const Buffer2D<Float3> &curFilteredColor) {
     }
     std::swap(m_misc, m_accColor);
 }
-
 Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     int height = frameInfo.m_beauty.m_height;
     int width = frameInfo.m_beauty.m_width;
     Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
+#define A_TROUS_WAVELET
+#ifdef A_TROUS_WAVELET
+    const static float a_trous_kernel[25] = {
+        1 / 256.0, 1 / 64.0, 3 / 128.0, 1 / 64.0, 1 / 256.0,
+        1 / 64.0, 1 / 16.0, 3 / 32.0, 1 / 16.0, 1 / 64.0,
+        3 / 128.0, 3 / 32.0, 9 / 64.0, 3 / 32.0, 3 / 128.0,
+        1 / 64.0, 1 / 16.0, 3 / 32.0, 1 / 16.0, 1 / 64.0,
+        1 / 256.0, 1 / 64.0, 3 / 128.0, 1 / 64.0, 1 / 256.0};
+    float stepWidth = 1.0f;
+    const int kernelRadius = 2;
+    const float c_phi = 1.0f, n_phi = 0.01f, p_phi = 0.1f;
+    Buffer2D<Float3> dstTmpImage = CreateBuffer2D<Float3>(width, height);
+    auto *src_color_map = &filteredImage;
+    auto *dst_color_map = &dstTmpImage;
+    for (int it = 0; it < 5; ++it) {
+#pragma omp parallel for
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                Float3 cval = (*src_color_map)(x, y);
+                Float3 nval = frameInfo.m_normal(x, y);
+                Float3 pval = frameInfo.m_position(x, y);
+                Float3 sum(0.f);
+                float cum_w = 0.0;
+                for (int i = -kernelRadius; i <= kernelRadius; ++i) {
+                    for (int j = -kernelRadius; j <= kernelRadius; ++j) {
+                        int nx = x + i * stepWidth, ny = y + j * stepWidth;
+                        if (nx >= 0 && nx < width && ny >= 0 && ny < height) {
+                            Float3 ctmp = it == 0?frameInfo.m_beauty(nx,ny):(*src_color_map)(nx, ny);
+                            Float3 t = cval - ctmp;
+                            float dist2 = Dot(t, t);
+                            float c_w = std::min(exp(-(dist2) / c_phi), 1.0f);
+
+                            Float3 ntmp = frameInfo.m_normal(nx, ny);
+                            t = nval - ntmp;
+                            dist2 = std::max(Dot(t, t) / (stepWidth * stepWidth), 0.0f);
+                            float n_w = std::min(exp(-(dist2) / n_phi), 1.0f);
+
+                            Float3 ptmp = frameInfo.m_position(nx, ny);
+                            t = pval - ptmp;
+                            dist2 = Dot(t, t);
+                            float p_w = std::min(exp(-(dist2) / p_phi), 1.0f);
+
+                            float weight = c_w * n_w * p_w * a_trous_kernel[(i + kernelRadius * 5) + j + kernelRadius];
+                            //weight /= stepWidth;
+                            sum += ctmp * weight;
+                            cum_w += weight;
+                        }
+                    }
+                }
+                if (cum_w != 0.0) {
+                    (*dst_color_map)(x, y) = sum / cum_w;
+                }
+            }
+        }
+
+        stepWidth *= 2.0f;
+        auto t = src_color_map;
+        src_color_map = dst_color_map;
+        dst_color_map = t;
+    }
+    return *dst_color_map;
+#else
     int kernelRadius = 16;
 #pragma omp parallel for
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             // TODO: Joint bilateral filter
             //filteredImage(x, y) = frameInfo.m_beauty(x, y);
-            
             // Done
             Float3 color = Float3(0.0);
             float weight = 0.0;
@@ -136,6 +196,7 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
         }
     }
     return filteredImage;
+#endif
 }
 
 void Denoiser::Init(const FrameInfo &frameInfo, const Buffer2D<Float3> &filteredColor) {
